cb4a72ea3c04862c9595dfdade1870ead4d81cf8
Aditya Raj Verma <adityarajverma@gmail.com>
2019-03-03 13:08:57 +0530

    Question 1 Solution

--- src/main/java/questions/Question1.java
+++ src/main/java/questions/Question1.java
@@ -15,10 +15,12 @@ public class Question1
 	{
 		int i=0;
 		int sum=0;
-		while(i<10)
+
+		while(i<1000)
 		{
 			if (i%3==0)
 			{
+				System.out.println(i);
 				sum=sum+i;
 
 			}
@@ -26,17 +28,43 @@ public class Question1
 
 			if (i%5==0)
 			{
+				System.out.println(i);
 				sum=sum+i;
 
 			}
 
+			if(i%15==0)
+			{
+				sum=sum-i;
+
+			}
+
+			i++;
+
 		}
 		System.out.println(sum);
 
-
-
 	}
 
+
+	/*
+	 * First of all, stop thinking on the number 1000 and turn your attention to the
+	 * number 990 instead. If you solve the problem for 990 you just have to add
+	 * 993,995,996 & 999 to it for the final answer. This sum is (a)=3983 Count all
+	 * the #s divisible by 3: From 3... to 990 there are 330 terms. The sum is
+	 * 330(990+3)/2, so (b)=163845
+	 *
+	 * Count all the #s divisible by 5: From 5... to 990 there are 198 terms. The
+	 * sum is 198(990+5)/2, so (c)=98505 Now, the GCD (greatest common divisor) of 3
+	 * & 5 is 1, so the LCM (least common multiple) should be 3×5=15.
+	 *
+	 * This means every number that divides by 15 was counted twice, and it should
+	 * be done only once. Because of this, you have an extra set of numbers started
+	 * with 15 all the way to 990 that has to be removed from (b)&(c).
+	 *
+	 * Then, from 15... to 990 there are 66 terms and their sum is 66(990+15)/2, so
+	 * (d)=33165 The answer for the problem is: (a)+(b)+(c)−(d)=233168
+	 */
 
 
 }
